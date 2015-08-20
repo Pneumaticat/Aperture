23a6ca7eb11d7c4e3cc417f16e263d94d460795c
Kevin Liu <batchfiles99@gmail.com>
2015-08-20 16:51:48 -0400

    Add more string utils tests

--- Aperture.Parser.Tests/StringHandlingTests.cs
+++ Aperture.Parser.Tests/StringHandlingTests.cs
@@ -43,6 +43,7 @@ namespace Aperture.Parser.Tests
             Assert.AreEqual(5, position); // Where position should end up in the string
         }
 
+        [TestMethod]
         public void TestCollectCharsFromMiddleOfStringUntilEOS()
         {
             int position = 5;
@@ -56,6 +57,70 @@ namespace Aperture.Parser.Tests
             Assert.IsTrue(position == testStr.Length); // position should be past end of string
                                                        // (remember: position is 0-based, length 
                                                        // is 1-based)
+        }
+
+        [TestMethod]
+        public void TestASCIICaseInsensitiveCompare()
+        {
+            Assert.IsTrue( StringUtils.CompareASCIICaseInsensitive("AAAA", "aaaa"));
+            // Unicode characters with different capitalization shouldn't 
+            // match using ascii comparing
+            Assert.IsFalse(StringUtils.CompareASCIICaseInsensitive("AAAAÆ", "aaaaæ"));
+        }
+
+        [TestMethod]
+        public void TestCompatibilityCaselessCompare()
+        {
+            Assert.IsTrue(StringUtils.CompareCompatibilityCaseless("Æ", "æ"));
+            // Compare composed e + ◌́ with é for equality. Should be equal if 
+            // normalization is correctly performed.
+            Assert.IsTrue(StringUtils.CompareCompatibilityCaseless(
+                "\u0065\u0301", "\u00e9"));
+        }
+
+        [TestMethod]
+        public void TestConvertToASCIIUpperAndLowercase()
+        {
+            Assert.AreEqual("AAA", StringUtils.ConvertToASCIIUppercase("aAa"));
+            Assert.AreEqual("aaa", StringUtils.ConvertToASCIILowercase("aAa"));
+            // Unicode characters should not be able to be lowercased or 
+            // uppercased with this method.
+            Assert.AreNotEqual("aæa", StringUtils.ConvertToASCIILowercase("aÆa"));
+            Assert.AreNotEqual("AÆA", StringUtils.ConvertToASCIIUppercase("aæa"));
+        }
+
+        [TestMethod]
+        public void TestSplittingStrings()
+        {
+            CollectionAssert.AreEqual(
+                new string[] { "x", "y", "c", "d" },
+                StringUtils.StrictlySplitString('|', "x|y|c|d"));
+            CollectionAssert.AreEqual(
+                new string[] { "csdf", "hi", "17" },
+                StringUtils.SplitStringOnCommas("  csdf, hi ,   17 ")
+            );
+        }
+
+        [TestMethod]
+        public void TestStripAndCollapseWhitespace()
+        {
+            Assert.AreEqual(
+                "kittens are awesome",
+                StringUtils.StripAndCollapseWhitespace(" kittens    are \r\n awesome\t")
+            );
+        }
+
+        [TestMethod]
+        public void TestTrimmingWhitespace()
+        {
+            Assert.AreEqual(
+                "x x",
+                StringUtils.TrimLeadingAndTrailingWhitespace("    x x   ")
+            );
+            Assert.AreEqual(
+                string.Empty,
+                StringUtils.TrimLeadingAndTrailingWhitespace("  \r\n\t    ")
+            );
         }
     }
 }
