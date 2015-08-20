10718fa0293a31fa1eca227525cfd6d714468ccc
Kevin Liu <batchfiles99@gmail.com>
2015-08-20 08:22:38 -0400

    Add whitespace floating point number test

---
+++ Aperture.Parser.Tests/FloatingPointUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aperture.Parser.Common;
+using Aperture.Parser.Exceptions;
+
+namespace Aperture.Parser.Tests
+{
+    [TestClass]
+    public class FloatingPointUnitTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidFloatNumberException),
+            "Whitespace was incorrectly allowed.")]
+        public void TestWhitespaceInallowance()
+        {
+            NumberUtils.ParseFloatingPointNumber(" ");
+            NumberUtils.ParseFloatingPointNumber("  ");
+            NumberUtils.ParseFloatingPointNumber("  E  ");
+        }
+    }
+}
