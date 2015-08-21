468559278478866570507be7f91339ae1e1042a8
Kevin Liu <batchfiles99@gmail.com>
2015-08-21 17:22:09 -0400

    Implement IsValidNormalisedLocalDateAndTimeString
    With accompanying tests, of course!

--- Aperture.Parser.Tests/DateTimeUtilsTests.cs
+++ Aperture.Parser.Tests/DateTimeUtilsTests.cs
@@ -186,5 +186,36 @@ namespace Aperture.Parser.Tests
                 DateTimeUtils.IsValidLocalDateAndTimeString("    "),
                 "Incorrectly allows only-whitespace string.");
         }
+
+        [TestMethod]
+        public void TestIsValidNormalisedLocalDateAndTimeString()
+        {
+            Assert.IsTrue(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00"));
+            Assert.IsTrue(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T23:59:59"));
+            Assert.IsTrue(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T23:59:59.3"));
+            Assert.IsTrue(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T23:59:59.777"));
+            // Spaces are not allowed!
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01 12:00"));
+            // Unnecessary and rejected seconds part
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00:00"));
+            // Seconds with unnecessary fractional part
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00:01.000"));
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00:01.00"));
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00:01.0"));
+            // Seconds with unnecessary trailing 0 in fractional part
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00:01.230"));
+            Assert.IsFalse(
+                DateTimeUtils.IsValidNormalisedLocalDateAndTimeString("2001-01-01T12:00:01.20"));
+        }
     }
 }
--- Aperture.Parser/Common/DateTimeUtils.cs
+++ Aperture.Parser/Common/DateTimeUtils.cs
@@ -323,5 +323,55 @@ namespace Aperture.Parser.Common
                    IsValidDateString(items[0]) &&
                    IsValidTimeString(items[1]);
         }
+        
+        public static bool IsValidNormalisedLocalDateAndTimeString(string input)
+        {
+            // Requirements:
+            // 1. A valid date string representing the date
+            // A T separator (only T)
+            // A valid time string representing the time, as short as possible
+            // (eg. if seconds is :00, do not specify seconds explicitly).
+            string[] items = input.Split('T');
+            if (items.Length == 2)
+            {
+                bool validDate = IsValidDateString(items[0]);
+                // If second half of input is valid time string...
+                if (timeStringRegex.IsMatch(items[1]))
+                {
+                    // Group 3 of regex: optional seconds
+                    // Group 4: optional fractional part of seconds
+                    GroupCollection groups = timeStringRegex.Match(items[1]).Groups;
+                    
+                    if (groups[3].Value == "00")
+                        // Seconds is just 0, but is explicitly written out as
+                        // :00, which is not as short as it can be.
+                        return false;
+                    else if (groups[4].Value != string.Empty &&
+                             groups[4].Value.TrimEnd('0') != groups[4].Value)
+                        // Fractional second is specified and there are 0s you 
+                        // can trim off the end. Not as short as it could beee.
+                        return false;
+                    else if (validDate)
+                        // Time string is as short as possible, date must also 
+                        // be valid for it to return true.
+                        return true;
+                    else
+                        // Date is not valid, return false.
+                        return false;
+                }
+                else
+                {
+                    // Time regex did not match the second half of the string, 
+                    // not a valid string.
+                    return false;
+                }
+            }
+            else
+            {
+                // Could not split the string by a 'T' and get 2 substrings, 
+                // invalid string.
+                return false;
+            }
+        }
     }
 }
