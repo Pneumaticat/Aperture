7c32ca837483b4c365eb1dd3bf8dc16d84a301c2
Kevin Liu <batchfiles99@gmail.com>
2015-08-21 07:23:50 -0400

    Simplify IsValidMonth string method

--- Aperture.Parser/Common/DateUtils.cs
+++ Aperture.Parser/Common/DateUtils.cs
@@ -11,45 +11,7 @@ namespace Aperture.Parser.Common
     {
 		public static bool IsValidMonthString(string input)
         {
-            int i = 0;
-            string yearDigits = StringUtils.CollectSequenceOfCharacters(
-				input, 
-				ref i, 
-				(char ch) => StringUtils.ASCIIDigits.Contains(ch));
-
-            if (yearDigits.Length < 4)
-                return false;
-
-            int yearNum = int.Parse(yearDigits);
-            if (yearNum < 1)
-                return false;
-
-            if (i >= input.Length)
-				// Hit end of string after year string - not valid
-                return false;
-
-            if (input[i] == '-')
-                i++;
-            else
-				// Next character after year is not a hyphen - not valid
-                return false;
-
-            // If next two characters are ASCII digits (month section)...
-            string potentialMonthString = input.Substring(i, 2);
-            if (potentialMonthString.All(c => StringUtils.ASCIIDigits.Contains(c)) &&
-                // and it is between 1 and 12
-                int.Parse(potentialMonthString) >= 1 &&
-                int.Parse(potentialMonthString) <= 12)
-            {
-                i = i + 2;
-                if (i >= input.Length)
-                    return true;
-                else
-					// Have not reached end of string after the month segment - not valid
-                    return false;
-            }
-            else
-                return false;
+            return ParseMonthString(input) != null;
         }
 
         // TODO: Should ParseMonthString return null upon failure?
