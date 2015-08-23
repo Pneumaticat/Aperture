45f1d3896dd64dbe2f97c7127d77144d5a6bdfb5
Kevin Liu <batchfiles99@gmail.com>
2015-08-23 07:48:58 -0400

    Implement IsValidGlobalDateAndTimeString

---
+++ Aperture.Parser.Tests/HTML/Microsyntaxes/DatesAndTimes/GlobalDatesAndTimesTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aperture.Parser.HTML.Microsyntaxes.DatesAndTimes;
+
+namespace Aperture.Parser.Tests.HTML.Microsyntaxes.DatesAndTimes
+{
+    [TestClass]
+    public class GlobalDatesAndTimesTests
+    {
+        [TestMethod]
+        public void TestIsValidGlobalDateAndTimeString()
+        {
+            Assert.IsTrue(GlobalDatesAndTimes.IsValidGlobalDateAndTimeString(
+                "2001-12-31T23:59:59.000Z"));
+            Assert.IsTrue(GlobalDatesAndTimes.IsValidGlobalDateAndTimeString(
+                "2001-12-31 00:00:00.000+00:15"));
+        }
+    }
+}
---
+++ Aperture.Parser/HTML/Microsyntaxes/DatesAndTimes/GlobalDatesAndTimes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aperture.Parser.HTML.Microsyntaxes.DatesAndTimes
+{
+    public static class GlobalDatesAndTimes
+    {
+        public static bool IsValidGlobalDateAndTimeString(string input)
+        {
+            int position = 0;
+            if (Dates.ParseDateComponent(input, ref position) == null)
+                return false;
+
+            if (position >= input.Length ||
+                (input[position] != 'T' && input[position] != ' '))
+                return false;
+            else
+                position++;
+
+            if (Times.ParseTimeComponent(input, ref position) == null)
+                return false;
+
+            if (TimeZones.ParseTimeZoneOffsetComponent(input, ref position) == null)
+                return false;
+
+            return true;
+        }
+    }
+}
